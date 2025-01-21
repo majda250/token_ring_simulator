9bef02b12bb67989d35e99d009de650b333b3eeb
majda250 <bendifimajda0@gmail.com>
2025-01-21 13:40:37 +0100

    Update atelierprojet11.c

--- atelierprojet11.c
+++ atelierprojet11.c
@@ -2,24 +2,30 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define NOMBRE_NOEUDS 5  
-#define JETON_INITIAL 1 
-int main() {
-    int canaux[NOMBRE_NOEUDS][2]; 
-    int jeton = JETON_INITIAL;  
-    pid_t pid;
-    for (int i = 0; i < NOMBRE_NOEUDS; i++) {
+#define MAX_NOEUDS 10
+#define JETON_INIT 1
+typedef struct {
+    char jeton[3];
+    int id_emetteur;
+} TrameJeton;
+bool est_noeud_actif(int id_noeud) {
+    return id_noeud != 2;
+}
+void simulation_anneau_jeton(int nb_noeuds) {
+    int canaux[MAX_NOEUDS][2];
+    pid_t pids[MAX_NOEUDS];
+    for (int i = 0; i < nb_noeuds; i++) {
         if (pipe(canaux[i]) == -1) {
-            perror("Erreur lors de la création du canal");
+            perror("Erreur lors de la creation du canal");
             exit(1);
         }
     }
-    for (int i = 0; i < NOMBRE_NOEUDS; i++) {
-        pid = fork();
+    for (int i = 0; i < nb_noeuds; i++) {
+        pid_t pid = fork();
         if (pid < 0) {
             perror("Erreur lors du fork");
             exit(1);
         }
-        if (pid == 0) {  
-            int jeton_recu;
+ 
